4373790bc089d79d90df5ed524a91c0fd9a1d163
alaaldin alhanini <98964675+Alaaldin98@users.noreply.github.com>
2022-03-16 16:22:47 +0200

    challengesone

--- Prework-CodeChallenges/Program.cs
+++ Prework-CodeChallenges/Program.cs
@@ -7,6 +7,39 @@ namespace Prework_CodeChallenges
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ArrayMaxResult();
+        }
+
+        static void ArrayMaxResult()
+        {
+            //  create an array with a size of 5 to have numbers in
+            int[] Nums = new int[5];
+
+            // tell user to input his array elements
+            Console.WriteLine("Please input 5 numbers between 1-10");
+
+            //loop 5 times and ask for input to add to the array
+            for (int i = 0; i < Nums.Length; i++)
+            {
+                Nums[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            //loop to print it
+            for (int i = 0; i < Nums.Length; i++)
+            {
+                Console.Write(Nums[i] + " ");
+            }
+
+            //Show user his array
+            Console.WriteLine("is your array, now select a number from the array!");
+
+            //store the inputted value into a
+            int a = Convert.ToInt32(Console.ReadLine());
+          if (a%2 == 0)   
+
+            Console.WriteLine($"Your computated score is: {a*2} ");
+         else   Console.WriteLine($"Your computated score is: {a} ");
         }
     }
 
